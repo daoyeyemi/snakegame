8e748a7949536dd23e78b7c9ce506ab2eb26c1ef
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-12-18 02:14:34 -0800

    added functions and classes and a few comments so I understand what is going on

---
+++ src/GameFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+    GameFrame() {
+        // add area where things like buttons checkboxes and textfields 
+        // can be added
+        this.add(new GamePanel());
+        // sets title of Frame / browser
+        this.setTitle("Snake");
+        // adds exit button
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // makes it so that user can't resize interface
+        this.setResizable(false);
+        // make frame so that it fits snugly around components within
+        this.pack();
+        // controls whether or not component is displayed onscreen
+        this.setVisible(true);
+        // centers to middle of whatever component it is in
+        this.setLocationRelativeTo(null); 
+    }
+}
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,55 @@
-import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 
+import javax.swing.JPanel;
+
+// GamePanel is inheriting  attributes and methods of JPanel
+// GamePanel is taking methods of ActionListener (allows for processing an
+// action event)
 public class GamePanel extends JPanel implements ActionListener {
     
     GamePanel() {
 
     }
 
+    public void startGame() {
+
+    }
+    // Graphics allows application to draw onto components
+    public void paintComponent(Graphics g) {
+        
+    }
+
+    public void draw(Graphics g) {
+        
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
+
+    // @override is used when we override a method in subclass
+    // in this case the actionPerformed method
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    // keyadapter is class for receiving keyboard events
+    public class MyKeyAdapter extends KeyAdapter {
+
+    }
 }
