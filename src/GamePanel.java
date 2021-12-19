46f8421b9d927d398f395b7f9d060de4b92b6b6a
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-12-19 02:48:22 -0800

    added more functions and variables alogn with grid and random "apple" appearing

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,7 +17,7 @@ public class GamePanel extends JPanel implements ActionListener {
     // final means constant; the value cannot change
     static final int SCREEN_HEIGHT = 600; 
     static final int SCREEN_WIDTH =  600;
-    static final int UNIT_SIZE = 25;
+    static final int UNIT_SIZE = 50;
     static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     static final int DELAY = 75;
     // square brackets [] indicate there is an array
@@ -34,19 +36,47 @@ public class GamePanel extends JPanel implements ActionListener {
     Random random;
 
     GamePanel() {
-
+        random = new Random();
+        // set the preferred size for each JPanel object
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        // set background settings
+        this.setBackground(Color.black);
+        // gives component option of being focused on or not
+        this.setFocusable(true);
+        // adds listener that is triggered when keys are pressed
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
 
     public void startGame() {
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
     // Graphics allows application to draw onto components
     public void paintComponent(Graphics g) {
+        // paints over parts that haven't been painted over
+        super.paintComponent(g);
+        // calls draw() method
+        draw(g);
+    }
+
+    public void draw(Graphics g) {        
+        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            // draws line in format (x1, x2, y1, y2)
+            g.setColor(Color.darkGray);
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        }
+        g.setColor(Color.RED);
+        g.fillOval(applesX, applesY, UNIT_SIZE, UNIT_SIZE);
         
     }
 
-    public void draw(Graphics g) {
-        
+    public void newApple() {
+        applesX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        applesY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
 
     public void move() {
